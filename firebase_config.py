577f80d34493120e228def83d4b5498d3efbc13d
John Joseph R <73868284+John-Joseph-R@users.noreply.github.com>
2024-08-31 20:59:54 +0530

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -2,7 +2,7 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 
 # Initialize Firebase connection
-cred = credentials.Certificate("medmoncredentials.json")
+cred = credentials.Certificate("path/to/serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
